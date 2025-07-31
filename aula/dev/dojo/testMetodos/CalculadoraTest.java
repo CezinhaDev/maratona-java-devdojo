60f35708a5d5693ee9244f0489ae6660e6ede3b2
CezinhaDev <cesarhenriquee04@gmail.com>
2025-07-31 13:48:08 -0300

    Orientação Objetos - Métodos

---
+++ aula/dev/dojo/testMetodos/CalculadoraTest.java
@@ -0,0 +1,13 @@
+package aula.dev.dojo.testMetodos;
+
+import aula.dev.dojo.javacore.Metodos.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+        System.out.println("Fim");
+
+    }
+}
